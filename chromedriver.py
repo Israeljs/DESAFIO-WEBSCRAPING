4b40f09294a16526427e6c3922e72f94921e7b53
israel jeronimo <silvaij16@gmail.com>
2023-08-22 23:01:25 -0300

    feat: busca apenas as notícias. Exclui as postagem sobre ofertas

--- chromedriver.py
+++ chromedriver.py
@@ -25,18 +25,18 @@ def get_posts(url):
     news_list = driver.find_elements(By.CSS_SELECTOR, ".list-item")
     posts = []
     for news in news_list:
-        news_items = fetch_posts(news)
+        post_category = news.find_element(By.CSS_SELECTOR, ".postCategory").text.find('OFERTAS')
 
-        posts.append(news_items)
+        if  post_category == -1:
+            news_items = fetch_posts(news)
+            posts.append(news_items)
 
     create_csv(posts)
 
     driver.quit()
 
-
 def main():
     get_posts("https://gizmodo.uol.com.br/")
-
 
 if __name__ == '__main__':
     main()
---
+++ t.py
@@ -0,0 +1,4 @@
+mensagem = 'Estou aprendendo Python na DevMedia'
+nova_mensagem = mensagem.slice(' ')
+print(nova_mensagem)
+
